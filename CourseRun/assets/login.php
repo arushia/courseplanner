3451246a67354fa8794274f2e32740ede2a2fd0f
swan-h <swanh2@illinois.edu>
2018-02-19 18:22:33 -0600

    login system

---
+++ CourseRun/assets/login.php
@@ -0,0 +1,119 @@
+<?php
+
+session_start();
+
+require 'database.php';
+
+if( isset($_SESSION['user_id']) ){
+
+	$records = $conn->prepare('SELECT id,email,password FROM users WHERE id = :id');
+	$records->bindParam(':id', $_SESSION['user_id']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$user = NULL;
+
+	if( count($results) > 0){
+		$user = $results;
+	}
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to your Web App</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<a href="/">Your App Name</a>
+	</div>
+
+	<?php if( !empty($user) ): ?>
+
+		<br />Welcome <?= $user['email']; ?>
+		<br /><br />You are successfully logged in!
+		<br /><br />
+		<a href="logout.php">Logout?</a>
+
+	<?php else: ?>
+
+		<h1>Please Login or Register</h1>
+		<a href="login.php">Login</a> or
+		<a href="register.php">Register</a>
+
+	<?php endif; ?>
+
+</body>
+</html>
+2. login.php
+
+PHP
+<?php
+
+session_start();
+
+if( isset($_SESSION['user_id']) ){
+	header("Location: /");
+}
+
+require 'database.php';
+
+if(!empty($_POST['email']) && !empty($_POST['password'])):
+
+	$records = $conn->prepare('SELECT id,email,password FROM users WHERE email = :email');
+	$records->bindParam(':email', $_POST['email']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$message = '';
+
+	if(count($results) > 0 && password_verify($_POST['password'], $results['password']) ){
+
+		$_SESSION['user_id'] = $results['id'];
+		header("Location: /");
+
+	} else {
+		$message = 'Sorry, those credentials do not match';
+	}
+
+endif;
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Below</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<a href="/">Your App Name</a>
+	</div>
+
+	<?php if(!empty($message)): ?>
+		<p><?= $message ?></p>
+	<?php endif; ?>
+
+	<h1>Login</h1>
+	<span>or <a href="register.php">register here</a></span>
+
+	<form action="login.php" method="POST">
+
+		<input type="text" placeholder="Enter your email" name="email">
+		<input type="password" placeholder="and password" name="password">
+
+		<input type="submit">
+
+	</form>
+
+</body>
+</html>

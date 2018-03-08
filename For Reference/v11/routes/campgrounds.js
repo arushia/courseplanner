909a0476cf2ffd55b026b4028afd5a53573f3c63
Michael Shea <mjshea3@illinois.edu>
2018-03-08 00:50:47 -0600

    started putting everything together

--- For Reference/v11/routes/campgrounds.js
+++ For Reference/v11/routes/campgrounds.js
@@ -41,7 +41,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 
 //NEW - show form to create new campground
 router.get("/new", middleware.isLoggedIn, function(req, res){
-   res.render("campgrounds/new"); 
+   res.render("campgrounds/new");
 });
 
 // SHOW - shows more info about one campground
@@ -91,4 +91,3 @@ router.delete("/:id",middleware.checkCampgroundOwnership, function(req, res){
 
 
 module.exports = router;
-
--- app.js
+++ app.js
@@ -8,15 +8,26 @@ var express     = require("express"),
     Section     = require("./models/sectionSchema"),
     User        = require("./models/userSchema"),
     seedDB      = require("./IR/JSONParser"),
-    getProfs    = require("./IR/getProfs")
+    flash       = require("connect-flash"),
+    path = require('path'),
+    methodOverride = require("method-override")
 
 mongoose.Promise = global.Promise;
 //mongoose.connect("mongodb://localhost/courseRun");
 mongoose.connect("mongodb://Mike:courserun@ds155288.mlab.com:55288/course-run")
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
 
+
+app.use(require("express-session")({
+    secret: "Mike is a pretty cool person",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -24,47 +35,137 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //seedDB();
-var profs = []
-Section.find({},(err,sections) => {
-  sections.forEach(function(ele){
-    ele.professors.forEach(function(prof){
-      profs.push(prof)
-      //console.log(prof)
-    })
-  })
-})
-
-var courses = []
-Course.find({},(err,courses) => {
-  courses.forEach(function(ele){
-    courses.push(ele.abrCollege + " "+ele.number)
-    //console.log(ele.abrCollege + " "+ele.number)
-  })
-})
-
-var colleges = []
-Course.find({},(err,courses) => {
-  courses.forEach(function(ele){
-    if(!colleges.includes(ele.abrCollege)){
-      colleges.push(ele.abrCollege)
-      console.log(ele.abrCollege)
-    }
-  })
-})
-
-
-
+// var profs = []
+// Section.find({},(err,sections) => {
+//   sections.forEach(function(ele){
+//     ele.professors.forEach(function(prof){
+//       profs.push(prof)
+//       //console.log(prof)
+//     })
+//   })
+// })
+//
+// var courses = []
+// Course.find({},(err,courses) => {
+//   courses.forEach(function(ele){
+//     courses.push(ele.abrCollege + " "+ele.number)
+//     //console.log(ele.abrCollege + " "+ele.number)
+//   })
+// })
+//
+// var colleges = []
+// Course.find({},(err,courses) => {
+//   courses.forEach(function(ele){
+//     if(!colleges.includes(ele.abrCollege)){
+//       colleges.push(ele.abrCollege)
+//       //console.log(ele.abrCollege)
+//     }
+//   })
+// })
+//
+// var courses = getCourses();
+// while(!courses){
+//   console.log("hi")
+// }
 
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("index.ejs");
+});
+app.get("/search", function(req, res){
+    res.render("search.ejs");
+});
+app.get("/register", function(req, res){
+    res.render("register.ejs");
+});
+app.get("/login", function(req, res){
+    res.render("login.ejs");
+});
+app.get("/accountsetup/:id", function(req, res){
+  Course.find({},function(err, Courses){
+    if(err){
+      console.log(err.message)
+    } else {
+      res.render("accountsetup.ejs", {user_id: req.params.id, courses: Courses});
+    }
+  })
+
+});
+app.get("/results/:lg/:hg/:colleges/:number/:professors", function(req, res){
+    res.render("course.ejs");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.post("/register", function(req, res){
+    var newUser = new User({email: req.body.username, username:req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            //req.flash("error", err.message);
+            console.log(err.message);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           //req.flash("success", "Welcome to CourseRun " + user.username+"!");
+           res.redirect("/accountsetup/"+user.id);
+        });
+    });
 });
 
-app.listen(process.env.PORT, process.env.IP);app.listen(3000, function(){
+app.put("/accountsetup/:id", function(req, res){
+   User.findByIdAndUpdate(req.params.id, {address: req.body.street+" "+req.body.town+" "+req.body.state+" "+req.body.zip}, function(err, user){
+      if(err){
+          res.redirect("index");
+      } else {
+          req.body.taken.forEach(function(ele){
+            var splitName = ele.split(" ")
+            Course.find({abrCollege:splitName[0], number:splitName[1]}, function(err,result){
+              if(err){
+                console.log(err.message)
+              } else {
+                if(result.length>0){
+                  user.coursesTaken.push(result[0])
+                  user.save()
+                }
+              }
+            })
+          })
+
+          req.body.favorited.forEach(function(ele){
+            var splitName = ele.split(" ")
+            Course.find({abrCollege:splitName[0], number:splitName[1]}, function(err,result){
+              if(err){
+                console.log(err.message)
+              } else {
+                if(result.length>0){
+                  user.favoritesPlanned.push(result[0])
+                  user.save()
+                }
+              }
+            })
+          })
+          res.redirect("/search");
+      }
+   });
+});
+
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/search",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+
+
+
+// app.get("/", function(req, res){
+//     res.render("home");
+// });
+//
+// app.get("/about", function(req, res){
+//     res.render("about");
+// });
+//
+app.listen(3000, function(){
    console.log("The Server Has Started!");
 });
 
